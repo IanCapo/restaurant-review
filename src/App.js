b741674ae756467e11586abeea8ba3cd28ad0789
Fabian Pasin <fabian.pasin@gmail.com>
2019-04-04 19:19:32 +0200

    changee app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import SimpleMap from '../src/components/Map/index'
-import List from '../src/components/Restaurants/index'
-import restaurants from './restaurants'
+import SimpleMap from '../src/components/Map/index.js'
+import List from '../src/components/Restaurants/index.js'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = this.getInitialstate()
+    this.state = {
+      restaurants: []
+    }
   }
-  // change restaurants.js to json and than fetch data inside componentDidMount
-  getInitialstate() {
-    return restaurants
+
+  componentDidMount = () => {
+    console.log('conponent did mount');
+    axios.get('./data.json')
+      .then((response) => {
+        console.log('response.data', response.data);
+        this.setState({ restaurants: response.data })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
-
+    console.log('this.state.restaurants', this.state.restaurants);
     return (
       <div className="App">
         <SimpleMap restaurants={this.state.restaurants} />
